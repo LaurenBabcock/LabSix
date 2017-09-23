685a7888133012668ea3b7ac4e8ce118094bf32d
loreinn13 <loreinn13@gmail.com>
2017-09-23 16:03:08 -0400

    Fixed problem with 2 letter words ending in vowels. Also, program keeps case of word entered now

--- Program.cs
+++ Program.cs
@@ -38,22 +38,18 @@ namespace LabSix
                     else
                     {                     
                         enteredWord = true;
-                        //Convert to lowercase
-                        userWord = userWord.ToLower();
+                        vowelPosition = FindVowel(userWord);
 
                         //If the word starts with a vowel, just add "way" on to the ending
-                        if (userWord.IndexOf("a") == 0 || userWord.IndexOf("e") == 0 || userWord.IndexOf("i") == 0 || userWord.IndexOf("o") == 0 || userWord.IndexOf("u") == 0)
+                        if (vowelPosition == 0)
                         {
                             pigLatinWord = userWord + "way";
 
                             Console.WriteLine("Your pig latin word is: " + pigLatinWord);
                         }
                         //If the word starts with a consonant, move all of the consonants that appear before the first vowel to the end of the word and add "ay" to the end
-                        else
+                        else if (vowelPosition > 0)
                         {
-                            //Determine the location of the first vowel using the FindVowel method
-                            vowelPosition = FindVowel(userWord);
-
                             //Create a substring of the consonants before the first vowel & a substring of the first vowel + all letters after it
                             lettersBeforeVowel = userWord.Substring(0, vowelPosition);
                             lettersFromVowelOn = userWord.Substring(vowelPosition);
@@ -64,6 +60,11 @@ namespace LabSix
                             //Display the pig latin word to the user
                             Console.WriteLine("Your pig latin word is: " + pigLatinWord);
 
+                        }
+                        //If there are no vowels, let the user know they may have mis-typed.
+                        else if (vowelPosition == -1)
+                        {
+                            Console.WriteLine("You may have misspelled your word. Your word does not have any vowels in it.");
                         }
                         //See if they would like to translate another word
                         Console.WriteLine();
@@ -78,24 +79,20 @@ namespace LabSix
 
         static int FindVowel(string aString)
         {
-            int vowelIndex = 0;
+            int vowelIndex = -1;
+            aString = aString.ToLower();
 
             //Change your string into an array of characters
             char[] individualChars = aString.ToCharArray();
 
             //Check each character to see if it is a vowel. As soon as the first vowel is found, return the index of that vowel.
-            for (int i = 0; i < individualChars.Length - 1; i++)
+            for (int i = 0; i <= individualChars.Length - 1; i++)
             {
                 if (individualChars[i] == 'a' || individualChars[i] == 'e' || individualChars[i] == 'i' || individualChars[i] == 'o' || individualChars[i] == 'u')
                 {
                     vowelIndex = i;
                     break;
                 }                           
-            }
-            //If there are no vowels, let the user know they may have mis-typed.
-            if (vowelIndex == 0)
-            {
-                Console.WriteLine("You may have misspelled your word. Your word does not have any vowels in it.");
             }
             return vowelIndex;
         }
